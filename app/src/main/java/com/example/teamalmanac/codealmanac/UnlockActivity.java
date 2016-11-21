43b2435838ec12a4ab1dae3657a5f6b0102588ec
sihyeon <ehpoos92@hanmail.net>
2016-11-21 18:47:45 +0900

    digital clock & datetime format setting

--- app/src/main/java/com/example/teamalmanac/codealmanac/UnlockActivity.java
+++ app/src/main/java/com/example/teamalmanac/codealmanac/UnlockActivity.java
@@ -1,14 +1,29 @@
 package com.example.teamalmanac.codealmanac;
 
+import android.graphics.Typeface;
+import android.renderscript.Type;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DigitalClock;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Typeface;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.example.teamalmanac.codealmanac.R.id.digitalclock;
+import static com.example.teamalmanac.codealmanac.R.id.textView;
+
 
 public class UnlockActivity extends AppCompatActivity {
     private ImageView mSlideIcon;
@@ -24,11 +39,26 @@ public class UnlockActivity extends AppCompatActivity {
         mMainLayout = (RelativeLayout)findViewById(R.id.activity_main);
         mSlideLayout = (RelativeLayout)findViewById(R.id.slideLayout);
         mSlideIcon = (ImageView)findViewById(R.id.imageView);
-        tv = (TextView)findViewById(R.id.textView);
+        tv = (TextView)findViewById(textView);
         Log.d("테스트", mMainLayout.getPaddingLeft()+"");
 
         mSlideLayout.setOnTouchListener(mSlideTouchListener);
+
+        //Digital Clock FONT asset
+        TextClock digitalClock = (TextClock) findViewById(R.id.digitalclock);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"digital-7.ttf");
+        digitalClock.setTypeface(typeface);
+
+        //datetime
+        TextView dt = (TextView)findViewById(R.id.datetime);
+        String format = new String("MM 월 dd일 EEEE");
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.KOREA);
+        dt.setText(sdf.format(new Date()));
+        Typeface type = Typeface.createFromAsset(getAssets(),"Spoqa_Han_Sans_Regular.ttf");
+        dt.setTypeface(type);
+
     }
+
     private View.OnTouchListener mSlideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
