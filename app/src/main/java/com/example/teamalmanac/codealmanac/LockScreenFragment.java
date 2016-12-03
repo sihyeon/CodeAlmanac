5df9098c59b9fb1abeece525dcdcd816bbb8dca4
sihyeon <ehpoos92@hanmail.net>
2016-12-03 17:57:26 +0900

    app1 TEXTVIEW 세부사항 배치 및 수정

--- app/src/main/java/com/example/teamalmanac/codealmanac/LockScreenFragment.java
+++ app/src/main/java/com/example/teamalmanac/codealmanac/LockScreenFragment.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.PermissionChecker;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
