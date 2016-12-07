bcee169e1f2b37752734d269b29f160ee8d81e28
sihyeon <ehpoos92@hanmail.net>
2016-12-08 03:38:01 +0900

    Pop액티비티+Appinfo액티비티 간 데이터 전송 연결 브렌치

--- app/src/main/java/com/example/teamalmanac/codealmanac/PopActivity.java
+++ app/src/main/java/com/example/teamalmanac/codealmanac/PopActivity.java
@@ -4,30 +4,56 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class PopActivity extends Activity{
     private Button popplus;
+    private TextView name;
+    private ImageView ic;
+    private GridView selected;
+    private Bitmap bitmap;
+    private String APP_PATH;
+    private String APP_ICON;
+
 @Override
     protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.popup);
 
+    selected = (GridView)findViewById(R.id.appgrid);
+    name = (TextView)findViewById(R.id.appname);
+    ic = (ImageView)findViewById(R.id.appicon);
 
+    //인텐트 받기
+    Intent get_intent = getIntent();
+    String APP_NAME = get_intent.getStringExtra("APP_NAME");
+//    get_intent.getStringExtra("APP_PATH");
+//    get_intent.getStringExtra("APP_ICON");
+
+//    Log.v("전달", APP_NAME);
+    //StringToBitMap(APP_ICON);
+    //ic.setImageBitmap(bitmap);
+//   name.setText(APP_NAME);
 
     // 전체 앱 불러오기
     popplus = (Button)this.findViewById(R.id.pop_btn);
@@ -39,5 +65,16 @@ public class PopActivity extends Activity{
             }
         });
 
+    }
+
+    public Bitmap StringToBitMap(String encodedString){
+        try{
+            byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
+            bitmap=BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }catch(Exception e){
+            e.getMessage();
+            return null;
+        }
     }
 }
